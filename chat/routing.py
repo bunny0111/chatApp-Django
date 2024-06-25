e2949720f761f0dec8c051728b4e933775d8eec1
bunny0111 <shubhamraj0111@gmail.com>
2024-06-25 11:56:45 +0530

    ChatApp Project

---
+++ chat/routing.py
@@ -0,0 +1,8 @@
+"""This will route the WebSocket connections to the consumers."""
+
+from django.urls import path, include
+from chat.consumers import ChatConsumer
+
+websocket_urlpatterns = [
+    path("", ChatConsumer.as_asgi()),
+]
